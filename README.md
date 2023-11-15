dcb009e2eaca1fb3177852d43f30bfc70a210061
AkumaYin0908 <buzterblaze0908@gmail.com>
2023-11-15 19:34:01 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+#coa-appearance-webapp
---
+++ src/main/java/com/coa/CoaappearanceApplication.java
@@ -0,0 +1,13 @@
+package com.coa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoaappearanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoaappearanceApplication.class, args);
+	}
+
+}
