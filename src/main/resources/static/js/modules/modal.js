16235732ceb3ca73b82e135af3554e05c899c7f1
AkumaYin0908 <buzterblaze0908@gmail.com>
2024-05-07 03:47:15 +0800

    remove the use of async in functions inside modal.js

--- src/main/resources/static/js/modules/modal.js
+++ src/main/resources/static/js/modules/modal.js
@@ -1,18 +1,18 @@
-export async function showMessage(messageHolderId, modalId) {
+export  function showMessage(messageHolderId, modalId) {
   let modalMessage = $(`#${messageHolderId}`).text();
   if (modalMessage != "") {
     $(`#${modalId}`).modal("show");
   }
 }
 
-export async function showModal(buttonId, modalId) {
+export  function showModal(buttonId, modalId) {
   $(`#${buttonId}`).on("click", function (event) {
     event.preventDefault();
     $(`#${modalId}`).modal("show");
   });
 }
 
-export async function hideModal(buttonId, modalId, fn = () => {}) {
+export  function hideModal(buttonId, modalId, fn = () => {}) {
   $(`#${buttonId}`).on("click", function (event) {
     event.preventDefault();
     $(`#${modalId}`).hide();
