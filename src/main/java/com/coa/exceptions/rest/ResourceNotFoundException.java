e78130743b71e10aa9fcc20c147fd4d1d03c7dfc
AkumaYin0908 <buzterblaze0908@gmail.com>
2024-05-14 19:33:55 +0800

    create ResourceNotFoundException class

---
+++ src/main/java/com/coa/exceptions/rest/ResourceNotFoundException.java
@@ -0,0 +1,31 @@
+package com.coa.exceptions.rest;
+
+import lombok.Data;
+
+@Data
+public class ResourceNotFoundException extends  RuntimeException {
+
+    private String resourceName;
+    private String fieldName;
+    private long longValue;
+    private String strValue;
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+
+    public ResourceNotFoundException(String resourceName, String fieldName, long longValue) {
+        super(String.format("%s not found with this %s : %d!",resourceName,fieldName,longValue));
+        this.resourceName = resourceName;
+        this.fieldName=fieldName;
+        this.longValue=longValue;
+    }
+
+    public ResourceNotFoundException(String resourceName,String fieldName,String strValue) {
+        super(String.format("%s not found with this %s : %s!",resourceName,fieldName,strValue));
+        this.resourceName = resourceName;
+        this.fieldName=fieldName;
+        this.strValue=strValue;
+    }
+
+}
