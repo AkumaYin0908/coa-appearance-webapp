743de1dbffa63cde313651478ceb3f366a075cf6
AkumaYin0908 <buzterblaze0908@gmail.com>
2024-05-14 19:33:40 +0800

    created APIResponse class

---
+++ src/main/java/com/coa/payload/response/APIResponse.java
@@ -0,0 +1,14 @@
+package com.coa.payload.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class APIResponse {
+
+    private String message;
+    private boolean success;
+}
