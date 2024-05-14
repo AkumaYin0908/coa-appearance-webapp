4f006e5e3c9effd71e11a66992203d41b1afbc45
AkumaYin0908 <buzterblaze0908@gmail.com>
2024-05-14 19:34:26 +0800

    create GlobalExceptionHandler and write methods that handle exceptions

---
+++ src/main/java/com/coa/exceptions/rest/GlobalExceptionHandler.java
@@ -0,0 +1,35 @@
+package com.coa.exceptions.rest;
+
+import com.coa.payload.response.APIResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<APIResponse> handleResourceNotFoundException(ResourceNotFoundException ex){
+        return new ResponseEntity<>(new APIResponse(ex.getMessage(), false), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String,String>> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex){
+        Map<String, String> response = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach(objectError -> {
+            String fieldName = ((FieldError)objectError).getField();
+            String message = objectError.getDefaultMessage();
+            response.put(fieldName, message);
+        });
+
+        return new ResponseEntity<>(response,HttpStatus.BAD_REQUEST);
+    }
+
+
+}
