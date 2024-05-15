49c74f671f99619d6d88e6bedd337cbd4411ec20
AkumaYin0908 <buzterblaze0908@gmail.com>
2024-05-16 05:50:43 +0800

    removed

--- src/main/java/com/coa/repository/rest/VisitorRepository.java
+++
@@ -1,8 +0,0 @@
-package com.coa.repository.rest;
-
-import com.coa.model.Visitor;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface VisitorRepository extends JpaRepository<Visitor,Long> {
-
-}
