ea2929b10c50c8e26ac0567d085d292aecd78b8d
AkumaYin0908 <buzterblaze0908@gmail.com>
2024-05-16 01:22:36 +0800

    create AddressConstant interface and declare variable which values are mySQL queries

---
+++ src/main/java/com/coa/constants/query_contants/AddressConstant.java
@@ -0,0 +1,18 @@
+package com.coa.constants.query_contants;
+
+public interface AddressConstant {
+
+    String findByName = "select * from address where name = ?";
+    String findByBarangay  = "select * from address where barangay = ? ";
+
+    String findByMunicipality = "select * from address where municipality = ?";
+
+    String findByProvince = "select * from address where province = ?";
+
+    String findByRegion = "select * from address where region = ?";
+
+    String findByCodes = "select * from address where (barangay is null or barangay = ?) and municipality = ? " +
+            "and province = ? and region = ?";
+
+
+}
