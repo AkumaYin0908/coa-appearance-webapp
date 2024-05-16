550c3cfe25374f217dfefd0e1e6a84e30435c23d
AkumaYin0908 <buzterblaze0908@gmail.com>
2024-05-16 16:56:08 +0800

    create AgencyService

---
+++ src/main/java/com/coa/service/AgencyService.java
@@ -0,0 +1,26 @@
+package com.coa.service;
+
+import com.coa.payload.request.AgencyRequest;
+import com.coa.payload.response.AgencyResponse;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+public interface AgencyService {
+
+    List<AgencyResponse> findAll();
+
+    AgencyResponse findById(Long id);
+
+    AgencyResponse findByName(String name);
+
+    Map<Long, String> findNames();
+
+    AgencyResponse save(AgencyRequest agency);
+
+    AgencyResponse update(Long id, AgencyRequest agency);
+
+    void delete(Long id);
+
+}
