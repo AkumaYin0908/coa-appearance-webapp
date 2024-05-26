53c2e0b7b01d506e124047e208027140fd83da80
AkumaYin0908 <buzterblaze0908@gmail.com>
2024-05-26 21:19:26 +0800

    replace @Data with @Getter and @Setter

--- src/main/java/com/coa/model/Appearance.java
+++ src/main/java/com/coa/model/Appearance.java
@@ -1,14 +1,13 @@
 package com.coa.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
