39b5ea8501f15e50637c0751e550f3282a9735ff
AkumaYin0908 <buzterblaze0908@gmail.com>
2024-05-30 01:22:08 +0800

    create PositionController with mapping methods

---
+++ src/main/java/com/coa/controller/rest/PositionController.java
@@ -0,0 +1,57 @@
+package com.coa.controller.rest;
+
+import com.coa.payload.request.PositionRequest;
+import com.coa.payload.response.APIResponse;
+import com.coa.payload.response.PositionResponse;
+import com.coa.service.PositionService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/positions")
+public class PositionController {
+
+    private final PositionService positionService;
+
+
+    @GetMapping("/all")
+    public ResponseEntity<List<PositionResponse>> getAllPosition(){
+        List<PositionResponse> positions = positionService.findAll();
+
+        if(positions.isEmpty()) return new ResponseEntity<>(new ArrayList<>(), HttpStatus.NO_CONTENT);
+
+        return new ResponseEntity<>(positions, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PositionResponse> getPositionById(@PathVariable("id")Long id){
+        return new ResponseEntity<>(positionService.findById(id), HttpStatus.FOUND);
+    }
+
+    @GetMapping
+    public ResponseEntity<PositionResponse> getPositionByTitle(@RequestParam("title")String title){
+        return new ResponseEntity<>(positionService.findByTitle(title), HttpStatus.FOUND);
+    }
+
+    @PostMapping
+    public ResponseEntity<PositionResponse> savePosition(@RequestBody PositionRequest positionRequest){
+        return new ResponseEntity<>(positionService.save(positionRequest), HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<PositionResponse> updatePosition(@PathVariable("id")Long id, @RequestBody PositionRequest positionRequest){
+        return  new ResponseEntity<>(positionService.update(id,positionRequest), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<APIResponse> deletePosition(@PathVariable("id")Long id){
+        positionService.delete(id);
+        return new ResponseEntity<>(new APIResponse("Deleted successfull!",true), HttpStatus.OK);
+    }
+}
