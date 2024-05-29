9f5f7a067936cd4da504c8ac5b86c5e90a4b6e72
AkumaYin0908 <buzterblaze0908@gmail.com>
2024-05-30 01:50:44 +0800

    edit query statement assigned to findTitles property

--- src/main/java/com/coa/constants/query_contants/PositionConstant.java
+++ src/main/java/com/coa/constants/query_contants/PositionConstant.java
@@ -4,5 +4,5 @@ public interface PositionConstant {
 
     String findByTitle = "select * from position where name = ?";
 
-    String findTitles = "select id, title, from position";
+    String findTitles = "select id, title from position";
 }
