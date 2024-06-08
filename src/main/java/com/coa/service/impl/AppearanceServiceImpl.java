6cbdeb11e1aa95caa92edc170ed97820173ef004
AkumaYin0908 <buzterblaze0908@gmail.com>
2024-06-08 15:48:38 +0800

    write method implementations of the overriden methods

--- src/main/java/com/coa/service/impl/AppearanceServiceImpl.java
+++ src/main/java/com/coa/service/impl/AppearanceServiceImpl.java
@@ -2,23 +2,35 @@ package com.coa.service.impl;
 
 import com.coa.exceptions.rest.ResourceNotFoundException;
 import com.coa.model.Appearance;
+import com.coa.model.Purpose;
 import com.coa.model.Visitor;
 import com.coa.payload.request.AppearanceRequest;
 import com.coa.payload.response.AppearanceResponse;
 import com.coa.repository.AppearanceRepository;
+import com.coa.repository.PurposeRepository;
 import com.coa.repository.VisitorRepository;
 import com.coa.service.AppearanceService;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class AppearanceServiceImpl implements AppearanceService {
     private final AppearanceRepository appearanceRepository;
+
+    private final VisitorRepository visitorRepository;
     private final ModelMapper modelMapper;
+
+    private final PurposeRepository purposeRepository;
+
+
+    private static final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("MMMM dd, yyyy");
 
 
     @Override
@@ -29,51 +41,97 @@ public class AppearanceServiceImpl implements AppearanceService {
 
     @Override
     public List<AppearanceResponse> findByVisitor(Long id) {
-        return null;
+
+        Visitor visitor = visitorRepository.findById(id).orElseThrow(()->new ResourceNotFoundException("Visitor","id",id));
+        return visitor.getAppearances().stream().map(appearance -> modelMapper.map(appearance,AppearanceResponse.class)).toList();
     }
 
     @Override
     public List<AppearanceResponse> findByVisitorAndDateIssued(Long id, String strDateIssued) {
-        return null;
+        Visitor visitor = visitorRepository.findById(id).orElseThrow(()->new ResourceNotFoundException("Visitor","id",id));
+
+        return visitor.getAppearances()
+                .stream()
+                .filter(appearance -> appearance.getDateIssued().equals(LocalDate.parse(strDateIssued,dateTimeFormatter)))
+                .map(appearance -> modelMapper.map(appearance,AppearanceResponse.class))
+                .toList();
     }
 
     @Override
     public List<AppearanceResponse> findByPurpose(String description) {
-        return null;
+        return appearanceRepository.findByPurpose(description).stream().map(appearance -> modelMapper.map(appearance,AppearanceResponse.class)).toList();
     }
 
     @Override
     public List<AppearanceResponse> findByDateIssued(String strDateIssued) {
-        return null;
+        return appearanceRepository.findByDateIssued(LocalDate.parse(strDateIssued,dateTimeFormatter)).stream()
+                .map(appearance -> modelMapper.map(appearance,AppearanceResponse.class)).toList();
     }
 
     @Override
     public List<AppearanceResponse> findByMonth(Integer month) {
-        return null;
+        return appearanceRepository.findByMonth(month).stream().map(appearance -> modelMapper.map(appearance,AppearanceResponse.class)).toList();
     }
 
     @Override
-    public List<AppearanceResponse> findByYear(Integer year) {
-        return null;
+    public List<AppearanceResponse> findByYear(Integer year){
+        return appearanceRepository.findByYear(year).stream().map(appearance -> modelMapper.map(appearance,AppearanceResponse.class)).toList();
     }
 
     @Override
     public AppearanceResponse findByReference(String reference) {
-        return null;
+        Appearance appearance = appearanceRepository.findByReference(reference).orElseThrow(()->new ResourceNotFoundException("Appearance","reference",reference));
+        return modelMapper.map(appearance,AppearanceResponse.class);
     }
 
     @Override
     public AppearanceResponse save(Long id, AppearanceRequest appearanceRequest) {
-        return null;
+
+        Visitor visitor = visitorRepository.findById(id).orElseThrow(()->new ResourceNotFoundException("Visitor","id",id));
+
+        Appearance appearance = new Appearance();
+        appearance.setVisitor(visitor);
+
+        appearance.setDateIssued(LocalDate.parse(appearanceRequest.getDateIssued(),dateTimeFormatter));
+        appearance.setDateFrom(LocalDate.parse(appearanceRequest.getDateFrom(),dateTimeFormatter));
+        appearance.setDateTo(LocalDate.parse(appearanceRequest.getDateTo(),dateTimeFormatter));
+
+        Purpose purpose = purposeRepository.findByDescription(appearanceRequest.getPurpose().getDescription())
+                .orElse(new Purpose(appearanceRequest.getPurpose().getDescription()));
+
+        purpose.addAppearance(appearance);
+
+        appearance.setReference(appearanceRequest.getReference());
+
+        Appearance dbAppearance = appearanceRepository.save(appearance);
+
+        return modelMapper.map(dbAppearance,AppearanceResponse.class);
+
+
     }
 
     @Override
     public AppearanceResponse update(Long id, AppearanceRequest appearanceRequest) {
-        return null;
+        Appearance appearance = appearanceRepository.findById(id).orElseThrow(()->new ResourceNotFoundException("Appearance","id",id));
+
+        appearance.setDateIssued(LocalDate.parse(appearanceRequest.getDateIssued(),dateTimeFormatter));
+        appearance.setDateFrom(LocalDate.parse(appearanceRequest.getDateFrom(),dateTimeFormatter));
+        appearance.setDateTo(LocalDate.parse(appearanceRequest.getDateTo(),dateTimeFormatter));
+
+        Purpose purpose = purposeRepository.findByDescription(appearanceRequest.getPurpose().getDescription())
+                .orElse(new Purpose(appearanceRequest.getPurpose().getDescription()));
+
+        purpose.addAppearance(appearance);
+
+        appearance.setReference(appearanceRequest.getReference());
+
+        appearanceRepository.save(appearance);
+
+        return  modelMapper.map(appearance, AppearanceResponse.class);
     }
 
     @Override
     public void delete(Long id) {
-
+        appearanceRepository.deleteById(id);
     }
 }
