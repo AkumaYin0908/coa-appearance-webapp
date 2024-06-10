ebeb9881d6bade3b9a5858219846560b7bbb3f55
AkumaYin0908 <buzterblaze0908@gmail.com>
2024-06-11 03:54:37 +0800

    refactor query of findByDescription constant

--- src/main/java/com/coa/constants/query_contants/PurposeConstant.java
+++ src/main/java/com/coa/constants/query_contants/PurposeConstant.java
@@ -2,7 +2,7 @@ package com.coa.constants.query_contants;
 
 public interface PurposeConstant {
 
-    String findByDescription = "select * from purpose where purpose = ?";
+    String findByDescription = "select * from purpose where description = ?";
 
     String findDescriptions = "select id, description from purpose";
 
