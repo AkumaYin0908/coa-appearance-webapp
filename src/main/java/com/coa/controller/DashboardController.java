2324d85b734216cbcb1e13fcdde9ea27365d7147
AkumaYin0908 <buzterblaze0908@gmail.com>
2024-06-11 03:59:52 +0800

    rename to DashboardController

---
+++ src/main/java/com/coa/controller/DashboardController.java
@@ -0,0 +1,5 @@
+package com.coa.controller;
+
+public class DashboardController {
+
+}
--- src/main/java/com/coa/controller/HomeController.java
+++
@@ -1,7 +0,0 @@
-package com.coa.controller;
-
-import org.springframework.web.bind.annotation.GetMapping;
-
-public class HomeController {
-
-}
