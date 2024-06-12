5183d2d02e2e415d94235c61e2139f7fa1ada723
AkumaYin0908 <buzterblaze0908@gmail.com>
2024-06-13 06:13:19 +0800

    create showDashBoard() method

--- src/main/java/com/coa/controller/DashboardController.java
+++ src/main/java/com/coa/controller/DashboardController.java
@@ -1,5 +1,15 @@
 package com.coa.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class DashboardController {
 
+
+    @GetMapping(value={"/","/dashboard"})
+    public String showDashboard(Model model){
+        return "dashboard";
+    }
 }
