a7b766344d220b1a7a8a5a6d50cbdce27afc125a
AkumaYin0908 <buzterblaze0908@gmail.com>
2024-07-03 05:27:21 +0800

    refactoring appearance-form.js

--- src/main/resources/static/js/appearance-form.js
+++ src/main/resources/static/js/appearance-form.js
@@ -1,56 +1,157 @@
 "use strict";
 import { datePickerSetting } from "./modules/date.js";
-import { visitorId } from "./visitor.js";
+import { visitorDetails } from "./modules/htmlContent.js";
+import { baseUrl } from "./modules/baseUrl.js";
+import { appearanceDetails } from "./modules/htmlContent.js";
+import { toast } from "./modules/alerts.js";
 
-$(document).ready(function () {
-  $("#dateIssued")
-    .datepicker(
-      $.extend({
-        defaultDate: new Date(),
-        altFormat: "yy-mm-dd",
-        dateFormat: "MM dd, yy",
-        changeMonth: true,
-        changeYear: true,
-        numberOfMonths: 1,
-      })
-    )
-    .datepicker("setDate", new Date());
+const visitorDetailContainer = $(".visitor-details");
+$(visitorDetails(visitor)).prependTo(visitorDetailContainer);
 
-  $(function () {
-    let dateFormat = "MM dd, yy",
-      dateFrom = $("#dateFrom")
-        .datepicker(datePickerSetting)
-        .datepicker("setDate", $("#dateIssued").datepicker("getDate"))
-        .on("change", function () {
-          dateTo.datepicker("option", "minDate", getDate(this));
-        }),
-      dateTo = $("#dateTo")
-        .datepicker(datePickerSetting)
-        .datepicker("setDate", dateFrom.datepicker("getDate"));
+const dateIssued = $("#dateIssued");
+const dateFrom = $("#dateFrom");
+const dateTo = $("#dateTo");
+const purpose = $("#purpose");
+const reference = $("#reference");
+const appearances = [];
+const isSingle = appearanceType === "single";
+console.log(appearanceType);
+$("#proceedButton").on("click", function (event) {
+  event.preventDefault();
+  if (isSingle) {
+    submitForm();
+  }
+});
 
-    function getDate(element) {
-      let date;
-      try {
-        date = $.datepicker.parseDate(dateFormat, element.value);
-      } catch (error) {
-        date = null;
+$("#appearances").on("click", "a.btn-delete", function (event) {
+  const id = $(this).parent().parent().attr("id")
+  const index = appearances.findIndex(appearance => appearance.dateFrom === id);
+
+  if(index !== -1){
+    appearances.splice(index,1);
+    $(this).parent().parent().remove();
+  }
+
+  console.log("Index of the deleted row: ",index);
+  console.log(appearances);
+  
+});
+
+function showAppearanceDetailModal(appearance) {
+  Swal.fire({
+    title: `Check the details before ${isSingle ? "printing" : "adding"}!`,
+    html: appearanceDetails(appearance),
+    showCancelButton: true,
+    confirmButtonText: isSingle ? "Print" : "Add",
+  })
+    .then((result) => {
+      if (result.isConfirmed) {
+        if (isSingle) {
+          submitFormToServer(appearance);
+        } else {
+          let reference = appearance.reference;
+          let dateFrom = appearance.dateFrom;
+          let dateTo = appearance.dateTo;
+          let purpose = appearance.purpose.description;
+
+          let row = `<tr id = "${dateFrom}">;
+                      <td>${reference}</td>
+                      <td>${dateFrom}</td>
+                      <td>${dateTo}</td>
+                      <td>${purpose}</td>
+                      <td><a type ="button" class = "btn-delete btn btn-sm btn-danger"><span class="material-symbols-outlined">delete</span></a>`;
+          $("#appearances tbody").append(row);
+          appearances.push(appearance);
+        }
       }
-      return date;
+    })
+    .catch((error) => {
+      Swal.fire({
+        title: "Error",
+        text: error.message,
+        icon: "error",
+      });
+    });
+}
+
+dateIssued
+  .datepicker(
+    $.extend({
+      defaultDate: new Date(),
+      altFormat: "yy-mm-dd",
+      dateFormat: "MM dd, yy",
+      changeMonth: true,
+      changeYear: true,
+      numberOfMonths: 1,
+    })
+  )
+  .datepicker("setDate", new Date());
+
+$(function () {
+  let dateFormat = "MM dd, yy",
+    dateFrom = $("#dateFrom")
+      .datepicker(datePickerSetting)
+      .datepicker("setDate", $("#dateIssued").datepicker("getDate"))
+      .on("change", function () {
+        dateTo.datepicker("option", "minDate", getDate(this));
+      }),
+    dateTo = $("#dateTo").datepicker(datePickerSetting).datepicker("setDate", dateFrom.datepicker("getDate"));
+
+  function getDate(element) {
+    let date;
+    try {
+      date = $.datepicker.parseDate(dateFormat, element.value);
+    } catch (error) {
+      date = null;
     }
-  });
+    return date;
+  }
+});
 
-  // $("#appearanceDetailsButton").on("click", function (event) {
-  //   event.preventDefault();
-  //   let dateIssued = $("#dateIssued").val();
-  //   let dateFrom = $("#dateFrom").val();
-  //   let dateTo = $("#dateTo").val();
-  //   let purpose = $("#purposeTextArea").val();
+function submitForm() {
+  let appearance = {
+    visitor: visitor,
+    dateIssued: dateIssued.val(),
+    dateFrom: dateFrom.val(),
+    dateTo: dateTo.val(),
+    purpose: {
+      description: purpose.val(),
+    },
+    reference: reference.val() != "" ? reference.val() : "N/A",
+  };
 
-  //   $("#dateIssuedDetail").text(dateIssued);
-  //   $("#dateFromDetail").text(dateFrom);
-  //   $("#dateToDetail").text(dateTo);
-  //   $("#purposeDetail").text(purpose);
+  showAppearanceDetailModal(appearance);
+}
 
-  //   $("#appearanceDetailsModal").modal("show");
-  // });
+function submitFormToServer(appearance) {
+  let id = appearance.visitor.id;
+  const fullUrl = `${baseUrl}/visitors/${id}/appearances`;
+
+  fetch(fullUrl, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(appearance),
+  })
+    .then((response) => {
+      if (!response.ok) {
+        return response.json().then((data) => {
+          throw new Error(data.message);
+        });
+      }
+      return response.json();
+    })
+    .then((data) => {
+      toast.fire({
+        icon: "success",
+        title: `New appearance for ${data.visitor.name} has been saved!`,
+      });
+    });
+}
+
+$(".btn-add").on("click", function (event) {
+  event.preventDefault();
+  submitForm();
+  console.log(appearances);
 });
