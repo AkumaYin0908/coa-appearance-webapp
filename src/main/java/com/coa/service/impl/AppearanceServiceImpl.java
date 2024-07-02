2ff0da12a8884e72386896901467b62acd658251
AkumaYin0908 <buzterblaze0908@gmail.com>
2024-07-03 05:23:17 +0800

    write implementation for saveAll() method;create map() method and refactor save() and update() method

--- src/main/java/com/coa/service/impl/AppearanceServiceImpl.java
+++ src/main/java/com/coa/service/impl/AppearanceServiceImpl.java
@@ -1,5 +1,6 @@
 package com.coa.service.impl;
 
+import com.coa.dto.AppearanceDTO;
 import com.coa.exceptions.rest.ResourceNotFoundException;
 import com.coa.model.Appearance;
 import com.coa.model.Purpose;
@@ -17,6 +18,7 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -36,113 +38,130 @@ public class AppearanceServiceImpl implements AppearanceService {
 
     @Override
     public AppearanceResponse findById(Long id) {
-        Appearance appearance = appearanceRepository.findById(id).orElseThrow(()->new ResourceNotFoundException("Appearance","id",id));
-        return modelMapper.map(appearance,AppearanceResponse.class);
+        Appearance appearance = appearanceRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Appearance", "id", id));
+        return modelMapper.map(appearance, AppearanceResponse.class);
     }
 
     @Override
     public List<AppearanceResponse> findAll() {
         return appearanceRepository.findAll().stream()
-                .map(appearance -> modelMapper.map(appearance,AppearanceResponse.class)).toList();
+                .map(appearance -> modelMapper.map(appearance, AppearanceResponse.class)).toList();
     }
 
     @Override
     public List<AppearanceResponse> findByVisitor(Long id) {
 
-        Visitor visitor = visitorRepository.findById(id).orElseThrow(()->new ResourceNotFoundException("Visitor","id",id));
-        return visitor.getAppearances().stream().map(appearance -> modelMapper.map(appearance,AppearanceResponse.class)).toList();
+        Visitor visitor = visitorRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Visitor", "id", id));
+        return visitor.getAppearances().stream().map(appearance -> modelMapper.map(appearance, AppearanceResponse.class)).toList();
     }
 
     @Override
     public List<AppearanceResponse> findByVisitorAndDateIssued(Long id, String strDateIssued) {
-        Visitor visitor = visitorRepository.findById(id).orElseThrow(()->new ResourceNotFoundException("Visitor","id",id));
+        Visitor visitor = visitorRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Visitor", "id", id));
 
         return visitor.getAppearances()
                 .stream()
-                .filter(appearance -> appearance.getDateIssued().equals(LocalDate.parse(strDateIssued,dateTimeFormatter)))
-                .map(appearance -> modelMapper.map(appearance,AppearanceResponse.class))
+                .filter(appearance -> appearance.getDateIssued().equals(LocalDate.parse(strDateIssued, dateTimeFormatter)))
+                .map(appearance -> modelMapper.map(appearance, AppearanceResponse.class))
                 .toList();
     }
 
     @Override
     public List<AppearanceResponse> findByPurpose(String description) {
 //        return appearanceRepository.findByPurpose(description).stream().map(appearance -> modelMapper.map(appearance,AppearanceResponse.class)).toList();
-        Purpose purpose = purposeRepository.findByDescription(description).orElseThrow(()->new ResourceNotFoundException("Purpose","description",description));
-        return purpose.getAppearances().stream().map(appearance -> modelMapper.map(appearance,AppearanceResponse.class)).toList();
+        Purpose purpose = purposeRepository.findByDescription(description).orElseThrow(() -> new ResourceNotFoundException("Purpose", "description", description));
+        return purpose.getAppearances().stream().map(appearance -> modelMapper.map(appearance, AppearanceResponse.class)).toList();
     }
 
     @Override
     public List<AppearanceResponse> findByDateIssued(String strDateIssued) {
-        return appearanceRepository.findByDateIssued(LocalDate.parse(strDateIssued,dateTimeFormatter)).stream()
-                .map(appearance -> modelMapper.map(appearance,AppearanceResponse.class)).toList();
+        return appearanceRepository.findByDateIssued(LocalDate.parse(strDateIssued, dateTimeFormatter)).stream()
+                .map(appearance -> modelMapper.map(appearance, AppearanceResponse.class)).toList();
     }
 
     @Override
     public List<AppearanceResponse> findByMonth(Integer month) {
-        return appearanceRepository.findByMonth(month).stream().map(appearance -> modelMapper.map(appearance,AppearanceResponse.class)).toList();
+        return appearanceRepository.findByMonth(month).stream().map(appearance -> modelMapper.map(appearance, AppearanceResponse.class)).toList();
     }
 
     @Override
-    public List<AppearanceResponse> findByYear(Integer year){
-        return appearanceRepository.findByYear(year).stream().map(appearance -> modelMapper.map(appearance,AppearanceResponse.class)).toList();
+    public List<AppearanceResponse> findByYear(Integer year) {
+        return appearanceRepository.findByYear(year).stream().map(appearance -> modelMapper.map(appearance, AppearanceResponse.class)).toList();
     }
 
     @Override
     public AppearanceResponse findByReference(String reference) {
-        Appearance appearance = appearanceRepository.findByReference(reference).orElseThrow(()->new ResourceNotFoundException("Appearance","reference",reference));
-        return modelMapper.map(appearance,AppearanceResponse.class);
+        Appearance appearance = appearanceRepository.findByReference(reference).orElseThrow(() -> new ResourceNotFoundException("Appearance", "reference", reference));
+        return modelMapper.map(appearance, AppearanceResponse.class);
     }
 
     @Override
     @Transactional
     public AppearanceResponse save(Long id, AppearanceRequest appearanceRequest) {
 
-        Visitor visitor = visitorRepository.findById(id).orElseThrow(()->new ResourceNotFoundException("Visitor","id",id));
+        Visitor visitor = visitorRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Visitor", "id", id));
 
         Appearance appearance = new Appearance();
         appearance.setVisitor(visitor);
 
-        appearance.setDateIssued(LocalDate.parse(appearanceRequest.getDateIssued(),dateTimeFormatter));
-        appearance.setDateFrom(LocalDate.parse(appearanceRequest.getDateFrom(),dateTimeFormatter));
-        appearance.setDateTo(LocalDate.parse(appearanceRequest.getDateTo(),dateTimeFormatter));
-
-        Purpose purpose = purposeRepository.findByDescription(appearanceRequest.getPurpose().getDescription())
-                .orElse(new Purpose(appearanceRequest.getPurpose().getDescription()));
-
-        purpose.addAppearance(appearance);
-
-        appearance.setReference(appearanceRequest.getReference());
+        map(appearance, appearanceRequest);
 
         Appearance dbAppearance = appearanceRepository.save(appearance);
 
-        return modelMapper.map(dbAppearance,AppearanceResponse.class);
+        return modelMapper.map(dbAppearance, AppearanceResponse.class);
 
+
+    }
+
+    @Override
+    public List<AppearanceResponse> saveAll(Long id, List<AppearanceRequest> appearanceRequests) {
+        List<Appearance> appearances = new ArrayList<>();
+        Visitor visitor = visitorRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Visitor", "id", id));
+
+        for (AppearanceRequest appearanceRequest : appearanceRequests) {
+            Appearance appearance = new Appearance();
+            appearance.setVisitor(visitor);
+
+            map(appearance, appearanceRequest);
+
+            appearances.add(appearance);
+        }
+
+        appearanceRepository.saveAll(appearances);
+
+        return appearances.stream().map(appearance -> modelMapper.map(appearance, AppearanceResponse.class)).toList();
 
     }
 
     @Override
     @Transactional
     public AppearanceResponse update(Long id, AppearanceRequest appearanceRequest) {
-        Appearance appearance = appearanceRepository.findById(id).orElseThrow(()->new ResourceNotFoundException("Appearance","id",id));
+        Appearance appearance = appearanceRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Appearance", "id", id));
 
-        appearance.setDateIssued(LocalDate.parse(appearanceRequest.getDateIssued(),dateTimeFormatter));
-        appearance.setDateFrom(LocalDate.parse(appearanceRequest.getDateFrom(),dateTimeFormatter));
-        appearance.setDateTo(LocalDate.parse(appearanceRequest.getDateTo(),dateTimeFormatter));
-
-        Purpose purpose = purposeRepository.findByDescription(appearanceRequest.getPurpose().getDescription())
-                .orElse(new Purpose(appearanceRequest.getPurpose().getDescription()));
-
-        purpose.addAppearance(appearance);
-
-        appearance.setReference(appearanceRequest.getReference());
+        map(appearance, appearanceRequest);
 
         appearanceRepository.save(appearance);
 
-        return  modelMapper.map(appearance, AppearanceResponse.class);
+        return modelMapper.map(appearance, AppearanceResponse.class);
     }
 
     @Override
     public void delete(Long id) {
         appearanceRepository.deleteById(id);
+    }
+
+
+    public void map(Appearance appearance, AppearanceRequest appearanceRequest) {
+        appearance.setDateIssued(LocalDate.parse(appearanceRequest.getDateIssued(), dateTimeFormatter));
+        appearance.setDateFrom(LocalDate.parse(appearanceRequest.getDateFrom(), dateTimeFormatter));
+        appearance.setDateTo(LocalDate.parse(appearanceRequest.getDateTo(), dateTimeFormatter));
+
+        Purpose purpose = purposeRepository.findByDescription(appearanceRequest.getPurpose().getDescription())
+                .orElse(new Purpose(appearanceRequest.getPurpose().getDescription()));
+
+        purpose.addAppearance(appearance);
+        purposeRepository.save(purpose);
+
+        appearance.setReference(appearanceRequest.getReference());
     }
 }
