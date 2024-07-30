e886ab48833f0ebc098cf894b5ff14e03dfa438f
AkumaYin0908 <buzterblaze0908@gmail.com>
2024-07-30 14:54:35 +0800

    create methods and write method implementation to overriden method

--- src/main/java/com/coa/service/certificate/impl/CertificateServiceImpl.java
+++ src/main/java/com/coa/service/certificate/impl/CertificateServiceImpl.java
@@ -1,19 +1,27 @@
 package com.coa.service.certificate.impl;
 
 import com.coa.payload.request.AppearanceRequest;
+import com.coa.payload.response.AgencyResponse;
 import com.coa.payload.response.AppearanceResponse;
+import com.coa.payload.response.VisitorResponse;
+import com.coa.payload.response.address.AddressResponse;
 import com.coa.service.certificate.CertificateService;
 import lombok.RequiredArgsConstructor;
-import net.sf.jasperreports.engine.JRException;
-import net.sf.jasperreports.engine.JasperExportManager;
-import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.*;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ResourceUtils;
 import org.springframework.util.StringUtils;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.Map;
 
 
 @Service
@@ -21,13 +29,70 @@ import java.nio.file.Paths;
 public class CertificateServiceImpl implements CertificateService {
 
 
+    private final ModelMapper modelMapper;
+
+
     //https://www.webscale.com/engineering-education/how-to-generate-reports-in-a-spring-boot-app-leveraging-jaspersoft/
-    private JasperPrint getJasperPrint(AppearanceResponse appearanceResponse){
-        return null;
+    private JasperPrint getJasperPrint(AppearanceResponse appearanceResponse, String resourceLocation) throws JRException, IOException{
+        File file = ResourceUtils.getFile(resourceLocation);
+        JasperReport jasperReport = JasperCompileManager.compileReport(file.getAbsolutePath());
+
+        Map<String, Object> parameters = map(appearanceResponse);
+
+        JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport,parameters,new JREmptyDataSource());
+        return jasperPrint;
     }
+
+    private Map<String, Object> map(AppearanceResponse appearanceResponse) {
+        Map<String, Object> parameters = new HashMap<>();
+
+        VisitorResponse visitorResponse = appearanceResponse.getVisitor();
+        AddressResponse addressResponse = visitorResponse.getAddress();
+
+
+        String dateOfTravel = getFormattedDateRange(appearanceResponse.getDateFrom(), appearanceResponse.getDateTo());
+
+
+        String fullName = String.format("%s%s%s", visitorResponse.getFirstName(),
+                visitorResponse.getMiddleInitial().equalsIgnoreCase("n/a") ? " " : visitorResponse.getMiddleInitial() + ". ",
+                visitorResponse.getLastName());
+
+        String fullAddress = String.format("%s%s, %s",addressResponse.getBarangay() == null ? "": String.format("%s, ",addressResponse.getBarangay().getName()),
+                addressResponse.getMunicipality().getName(), addressResponse.getProvince().getName());
+
+        String office = visitorResponse.getAgency() == null ? fullAddress : visitorResponse.getAgency().getName();
+
+        LocalDate dateIssued = LocalDate.parse(appearanceResponse.getDateIssued());
+
+        parameters.put("courtesyTitle",visitorResponse.getCourtesyTitle().getTitle());
+        parameters.put("fullName",fullName);
+        parameters.put("position",visitorResponse.getPosition().getTitle());
+        parameters.put("office",office);
+        parameters.put("dateOfTravel",dateOfTravel);
+        parameters.put("purpose", appearanceResponse.getPurpose().getDescription());
+        parameters.put("reference",appearanceResponse.getReference());
+        parameters.put("day",getDayWithOrdinal(dateIssued.getDayOfMonth()));
+        parameters.put("month", String.format("%s%s",dateIssued.getMonth().toString().charAt(0),
+                dateIssued.getMonth().toString().toLowerCase().substring(1)));
+
+        parameters.put("year",dateIssued.getYear());
+        parameters.put("lastName",visitorResponse.getLastName());
+
+        return parameters;
+
+
+    }
+
     @Override
-    public String generateSingleCertificate(AppearanceRequest appearanceRequest) throws JRException, IOException {
-        return null;
+    public String generateSingleCertificate(AppearanceRequest appearanceRequest,int templateNo) throws JRException, IOException {
+        String resourceLocation = String.format("classpath: /static/certificates/CA_Template_%d.jrxml",templateNo);
+
+        AppearanceResponse appearanceResponse = modelMapper.map(appearanceRequest,AppearanceResponse.class);
+        JasperPrint jasperPrint = getJasperPrint(appearanceResponse,resourceLocation);
+
+        String fileName = "certificate.pdf";
+        Path uploadPath = getUploadPath("pdf",jasperPrint,fileName);
+        return String.format("%s/%s",uploadPath.toString(),fileName);
     }
 
     private Path getUploadPath(String fileFormat, JasperPrint jasperPrint, String fileName) throws IOException, JRException{
@@ -45,6 +110,56 @@ public class CertificateServiceImpl implements CertificateService {
 
         return uploadPath;
 
+    }
+
+    private String getFormattedDateRange(String strDateFrom, String strDateTo) {
+
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("MMMM d, yyyy");
+        DateTimeFormatter dateFromFormat = DateTimeFormatter.ofPattern("MMMM d");
+        DateTimeFormatter dateToFormat = DateTimeFormatter.ofPattern("d, yyyy");
+
+        LocalDate dateFrom = LocalDate.parse(strDateFrom);
+        System.out.println(dateFrom);
+        LocalDate dateTo = LocalDate.parse(strDateTo);
+        System.out.println(dateTo);
+        String firstDate = "";
+        String lastDate = "";
+
+        String formattedDateRange = "";
+
+        LocalDate[] dates = {dateFrom, dateTo};
+
+        if (dateFrom.getYear() == dateTo.getYear()) {
+            if (dateFrom.getMonth().equals(dateTo.getMonth())) {
+                if (dateFrom.equals(dateTo)) {
+                    formattedDateRange = dateTimeFormatter.format(dateFrom);
+                } else {
+                    firstDate = dateFromFormat.format(dates[0]);
+                    lastDate = dateToFormat.format(dates[1]);
+                    formattedDateRange = String.format("%s - %s", firstDate, lastDate);
+                }
+            } else {
+                firstDate = dateFromFormat.format(dates[0]);
+                lastDate = dateTimeFormatter.format(dates[1]);
+                formattedDateRange = String.format("%s - %s", firstDate, lastDate);
+            }
+        } else {
+            firstDate = dateTimeFormatter.format(dates[0]);
+            lastDate = dateTimeFormatter.format(dates[1]);
+            formattedDateRange = String.format("%s - %s", firstDate, lastDate);
+        }
+
+        return formattedDateRange;
+    }
+
+    private static String getDayWithOrdinal(int day){
+
+        String suffix = "th";
+        if(day%10 == 1 && day%100 != 11) suffix = "st";
+        if(day%10 == 2 && day%100 != 12) suffix =  "nd";
+        if(day%10 == 3 && day%100 != 13) suffix = "rd";
+
+        return String.format("%d%s", day,suffix);
     }
 
 
