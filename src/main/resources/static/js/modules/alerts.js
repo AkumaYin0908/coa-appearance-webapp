ab9eb071412a0165db35bb635e6e472046cc2a15
AkumaYin0908 <buzterblaze0908@gmail.com>
2024-07-31 05:38:46 +0800

    reduce time of toast component

--- src/main/resources/static/js/modules/alerts.js
+++ src/main/resources/static/js/modules/alerts.js
@@ -2,7 +2,7 @@ export const toast = Swal.mixin({
   toast: true,
   position: "top-end",
   showConfirmButton: false,
-  timer: 3000,
+  timer: 500,
   timerProgressBar: true,
   didOpen: (toast) => {
     toast.onmouseenter = Swal.stopTimer;
