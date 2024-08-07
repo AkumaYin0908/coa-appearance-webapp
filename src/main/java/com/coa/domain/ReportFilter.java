6efa9d4001e423f2653440f081eb497729e07d8b
AkumaYin0908 <buzterblaze0908@gmail.com>
2024-08-07 08:15:38 +0800

    create ReportFilter class

---
+++ src/main/java/com/coa/domain/ReportFilter.java
@@ -0,0 +1,11 @@
+package com.coa.domain;
+
+
+import com.coa.domain.report.ExportType;
+
+//https://github.com/jamilxt/java_spring-boot_japser-report
+public class ReportFilter {
+
+    private ExportType exportType;
+
+}
