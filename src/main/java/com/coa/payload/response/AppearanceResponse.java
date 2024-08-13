c5ff012cbe351bd783cfa810d114e46071043b91
AkumaYin0908 <buzterblaze0908@gmail.com>
2024-08-13 16:36:43 +0800

    cut getFormattedDateRange() method

--- src/main/java/com/coa/payload/response/AppearanceResponse.java
+++ src/main/java/com/coa/payload/response/AppearanceResponse.java
@@ -37,44 +37,4 @@ public class AppearanceResponse implements Reportable {
         return Objects.hash(id);
     }
 
-
-    public String getFormattedDateRange() {
-        String strDateFrom = this.dateFrom;
-        String strDateTo = this.dateTo;
-        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("MMMM d, yyyy");
-        DateTimeFormatter dateFromFormat = DateTimeFormatter.ofPattern("MMMM d");
-        DateTimeFormatter dateToFormat = DateTimeFormatter.ofPattern("d, yyyy");
-
-        LocalDate dateFrom = LocalDate.parse(strDateFrom, dateTimeFormatter);
-        LocalDate dateTo = LocalDate.parse(strDateTo, dateTimeFormatter);
-
-        String firstDate = "";
-        String lastDate = "";
-
-        String formattedDateRange = "";
-
-        LocalDate[] dates = {dateFrom, dateTo};
-
-        if (dateFrom.getYear() == dateTo.getYear()) {
-            if (dateFrom.getMonth().equals(dateTo.getMonth())) {
-                if (dateFrom.equals(dateTo)) {
-                    formattedDateRange = dateTimeFormatter.format(dateFrom);
-                } else {
-                    firstDate = dateFromFormat.format(dates[0]);
-                    lastDate = dateToFormat.format(dates[1]);
-                    formattedDateRange = String.format("%s - %s", firstDate, lastDate);
-                }
-            } else {
-                firstDate = dateFromFormat.format(dates[0]);
-                lastDate = dateTimeFormatter.format(dates[1]);
-                formattedDateRange = String.format("%s - %s", firstDate, lastDate);
-            }
-        } else {
-            firstDate = dateTimeFormatter.format(dates[0]);
-            lastDate = dateTimeFormatter.format(dates[1]);
-            formattedDateRange = String.format("%s - %s", firstDate, lastDate);
-        }
-
-        return formattedDateRange;
-    }
 }
