cde0c36cf6348a6180685c93dd2858b2698e3feb
AkumaYin0908 <buzterblaze0908@gmail.com>
2024-08-18 20:07:11 +0800

    paste getLongDate() function and export it

--- src/main/resources/static/js/modules/date.js
+++ src/main/resources/static/js/modules/date.js
@@ -5,3 +5,8 @@ export const datePickerSetting = {
   changeYear: true,
   numberOfMonths: 1,
 };
+
+export function getLongDate(date) {
+  const dateFormatSetting = { year: "numeric", month: "long", day: "numeric" };
+  return new Date(date).toLocaleDateString("EN-US", dateFormatSetting);
+}
