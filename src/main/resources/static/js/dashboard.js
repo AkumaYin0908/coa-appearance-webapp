b99b73d97d9281f762c6e2ecdc46cfaf3ffa3b9f
AkumaYin0908 <buzterblaze0908@gmail.com>
2024-08-21 20:45:30 +0800

    empty dashboard.js leaving only import statements

--- src/main/resources/static/js/dashboard.js
+++ src/main/resources/static/js/dashboard.js
@@ -1,45 +1,5 @@
 "use strict";
 
-import { showMessage, showModal, hideModal } from "./modules/modal.js";
 import { baseUrl } from "./modules/baseUrl.js";
+import { displayCurrentLeader } from "./modules/html-content.js";
 
-$(document).ready(function () {
-  let fullUrl = `${baseUrl}/appearances`;
-
-  new DataTable("#history", {
-    ajax: fullUrl,
-    columns: [
-      { data: "visitor" },
-      { data: "dateIssued" },
-      { data: "dateFrom" },
-      { data: "dateTo" },
-      { data: "purpose" },
-      { data: "reference" },
-    ],
-  });
-  $("#dashboardButton").addClass("active");
-
-  showMessage("addModalMessageHolder", "addVisitorModal");
-
-  showMessage("changeModalMessageHolder", "changeLeaderModal");
-
-  showModal("changeLeaderButton", "changeLeaderModal");
-
-  const clearChangeModal = function () {
-    $("#currentLeaderName").val("");
-    $("#leaderName").val("");
-  };
-
-  hideModal("changeModalCloseButton", "changeModalDiv", clearChangeModal);
-
-  showModal("addVisitorButton", "addVisitorModal");
-
-  const clearAddModal = function () {
-    $("#visitorId").val("");
-    $("#visitorName").val("");
-    $("#visitorPosition").val("");
-    $("#visitorAgency").val("");
-  };
-
-  hideModal("addModalCloseButton", "addModalDiv", clearAddModal);
-});
