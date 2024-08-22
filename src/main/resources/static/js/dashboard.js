cd390f231055f1a05b63d62e86243729ef08bed1
AkumaYin0908 <buzterblaze0908@gmail.com>
2024-08-22 21:41:39 +0800

    create dataTable constant

--- src/main/resources/static/js/dashboard.js
+++ src/main/resources/static/js/dashboard.js
@@ -1,5 +1,7 @@
 "use strict";
 
-import { baseUrl } from "./modules/baseUrl.js";
+import { baseUrl } from "./modules/base-url.js";
 import { displayCurrentLeader } from "./modules/html-content.js";
+import { appearanceHistoryTableObject } from "./modules/table-object.js";
 
+const dataTable = await $("#appearance").DataTable(appearanceHistoryTableObject(`${baseUrl}/appearances/DESC`));
