b5b76e637f8f886d8d85390705ce017ca7bd31d6
AkumaYin0908 <buzterblaze0908@gmail.com>
2024-08-22 21:43:22 +0800

    paste getFormattedDateRange() method

--- src/main/java/com/coa/payload/response/AppearanceResponse.java
+++ src/main/java/com/coa/payload/response/AppearanceResponse.java
@@ -3,6 +3,7 @@ package com.coa.payload.response;
 
 import com.coa.model.Agency;
 import com.coa.payload.Reportable;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -35,6 +36,45 @@ public class AppearanceResponse implements Reportable {
     @Override
     public int hashCode() {
         return Objects.hash(id);
+    }
+
+    @JsonProperty("formattedDateRange")
+    public String getFormattedDateRange() {
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("MMMM d, yyyy");
+        DateTimeFormatter dateFromFormat = DateTimeFormatter.ofPattern("MMMM d");
+        DateTimeFormatter dateToFormat = DateTimeFormatter.ofPattern("d, yyyy");
+
+        LocalDate dateFrom = LocalDate.parse(this.dateFrom);
+        LocalDate dateTo = LocalDate.parse(this.dateTo);
+
+        String firstDate = "";
+        String lastDate = "";
+
+        String formattedDateRange = "";
+
+        LocalDate[] dates = {dateFrom, dateTo};
+
+        if (dateFrom.getYear() == dateTo.getYear()) {
+            if (dateFrom.getMonth().equals(dateTo.getMonth())) {
+                if (dateFrom.equals(dateTo)) {
+                    formattedDateRange = dateTimeFormatter.format(dateFrom);
+                } else {
+                    firstDate = dateFromFormat.format(dates[0]);
+                    lastDate = dateToFormat.format(dates[1]);
+                    formattedDateRange = String.format("%s - %s", firstDate, lastDate);
+                }
+            } else {
+                firstDate = dateFromFormat.format(dates[0]);
+                lastDate = dateTimeFormatter.format(dates[1]);
+                formattedDateRange = String.format("%s - %s", firstDate, lastDate);
+            }
+        } else {
+            firstDate = dateTimeFormatter.format(dates[0]);
+            lastDate = dateTimeFormatter.format(dates[1]);
+            formattedDateRange = String.format("%s - %s", firstDate, lastDate);
+        }
+
+        return formattedDateRange;
     }
 
 }
